8baa2065e72abd7e8761e74fb90bb8a3111f145d
alqio <kyosti.alkio@gmail.com>
2021-02-17 10:17:41 +0200

    init

---
+++ Dark Game/Assets/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Collider2D outCollider;
+    public Collider2D inCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
