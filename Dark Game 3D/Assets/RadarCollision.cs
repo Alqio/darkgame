b5d8470e2c7fa6135d5749b7187df30d727d7fec
Tomi Panula <tomipanula1@gmail.com>
2021-02-19 19:45:45 +0200

    3d

---
+++ Dark Game 3D/Assets/RadarCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarCollision : MonoBehaviour
+{
+    ContactPoint[] contacts = new ContactPoint[1000];
+    public LineRenderer lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lr = GetComponent<LineRenderer>();
+        Physics.reuseCollisionCallbacks = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics
+    }
+
+    void OnCollisionStay(Collision col){
+        col.GetContacts(contacts);
+        //Debug.Log(contacts.Length);
+        Debug.Log(col.contactCount);
+/*
+        foreach (var item in contacts)
+        {
+            Debug.Log(item.point);
+        }
+  */      
+        lr.positionCount = contacts.Length;
+        for(int i = 0; i < contacts.Length; i++)
+        {   
+            lr.SetPosition(i, contacts[i].point);
+        }
+    }
+}
---
+++ Dark Game 3D/Assets/RadarScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarScript : MonoBehaviour
+{
+    public int rayCount = 100;
+    private Mesh mesh;
+    public int rayLayerCount = 9;
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    void Update()
+    {
+        
+
+        float horizontalAngle = 0f;
+        float horizontalAngleIncrease= 360f / rayCount;
+        float verticalAngle = 45f;
+        float verticalAngleIncrease = 90f / rayLayerCount;
+        
+        for(int i = 0; i < rayLayerCount; i++){
+            for(int j = 0; j < rayCount; j++) {
+                //Debug.DrawRay(transform.position, GetVectorFromAngle(verticalAngle, horizontalAngle) * 10f, Color.green);
+                horizontalAngle += horizontalAngleIncrease;
+            }
+            verticalAngle -= verticalAngleIncrease;
+        }
+    }
+
+    private Vector3 GetVectorFromAngle(float verticalAngle, float horizontalAngle)
+    {
+        float angleRadVertical = verticalAngle * (Mathf.PI / 180f);
+        float angleRadHorizontal = horizontalAngle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(horizontalAngle),Mathf.Sin(verticalAngle), Mathf.Sin(horizontalAngle));
+    }
+}
